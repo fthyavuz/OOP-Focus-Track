d96b075cb8067874ee680a9e366af276b98dab55
Fatih Yavuz <fatih.yavuz@powercoders.org>
2022-11-21 11:08:31 +0100

    First Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+static int calSquare(int num)
+{
+    return num * num;
+}
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(calSquare(3));
